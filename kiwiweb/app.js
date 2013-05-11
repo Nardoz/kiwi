2897d1b75a96a6cf00e751e7136992d33bd81b0c
Pala <paladinop@gmail.com>
2013-05-11 17:29:54 -0300

    routes

--- kiwiweb/app.js
+++ kiwiweb/app.js
@@ -33,7 +33,8 @@ app.configure('development', function(){
 });
 
 app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('/usuarios', routes.usuarios);
+app.get('/estaciones', riutes.estaciones);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
