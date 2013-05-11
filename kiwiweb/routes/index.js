850b3e88f0c302535a162bf8a39b0dfaf714bf1f
Nicolás Melendez <nfmelendez@gmail.com>
2013-05-11 17:32:04 -0300

    finish routes

--- kiwiweb/routes/index.js
+++ kiwiweb/routes/index.js
@@ -4,5 +4,13 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express', page: 'pages/index.ejs' });
+  res.render('index', { title: 'The Kiwi Page', page: 'pages/index.ejs' });
+};
+
+exports.estaciones = function(req, res){
+  res.render('index', { title: 'Estaciones', page: 'pages/estaciones.ejs' });
+};
+
+exports.usuarios = function(req, res){
+  res.render('index', { title: 'Usuarios', page: 'pages/usuarios.ejs' });
 };
