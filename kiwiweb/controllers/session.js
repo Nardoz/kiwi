d90199722ddaa68aa9e2f12427d6bb55e941715a
Pala <paladinop@gmail.com>
2013-05-12 13:19:25 -0300

    doing some Pala magic...

---
+++ kiwiweb/controllers/session.js
@@ -0,0 +1,15 @@
+var db = require('../lib/db');
+var Promise = require('../lib/promise');
+var CONS = require('../lib/cons')
+var sessionService = require('../services/session');
+
+exports.getById = function(req, res, next) {
+	Promise.all([
+		db.findOne('session', {id: parseInt(req.params.id, 10)})
+	])
+	.spread(function(session){
+		res.render('session', { title: 'Session Details ', session: session });
+	})
+	.done();
+};
+
--- kiwiweb/routes.js
+++ kiwiweb/routes.js
@@ -15,6 +15,8 @@ module.exports = function(app) {
 	app.get('/stations', controllers.station.list);
 	app.get('/stations/:id', controllers.station.getById);
 
+	//app.get('/sessions/:id', controllers.session.getById);
+
 	// SMS
 	app.post('/api/users/:userId/sessions', controllers.api.createSession);
 
