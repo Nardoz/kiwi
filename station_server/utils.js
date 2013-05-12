5872a3f29054bf6ac40413ad2f1edb7552086733
Alan Reid <mail@alanreid.com.ar>
2013-05-12 16:33:35 -0300

    refactor and completed functionality

--- station_server/utils.js
+++ station_server/utils.js
@@ -1,10 +1,25 @@
 
+var request = require('request'),
+    config  = require('./config.json');
+
 module.exports = {
+
   slotIsValid: function(station, slot) {
     var parsed = this.parseSlot(slot);
     return parsed !== null && parsed.length === 3 && parsed[1] === station;
   },
+
   parseSlot: function(slotId) {
     return slotId.match(/([0-9]{2})([0-9]{2})/);
+  },
+
+  sendRequest: function(action, data) {
+
+    if(data === undefined) {
+      data = {};
+    }
+
+    request.put(config.kiwi.hostname + ':' + config.kiwi.port + '/api' + action, { json: data });
   }
+
 };
